f669f02b6b051ed4a9e4cb5e8922493aa940815f
henningfutrell <henning.futrell@protonmail.com>
2018-07-05 14:40:43 -0400

    Remove silent updating.

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 export update_basically_powerful_zsh () {
     cd $z_core
-    git pull update master 1&> /dev/null
-    git submodule update --init --recursive 1&> /dev/null
+    git pull update master
+    git submodule update --init --recursive
     cd ~
 }
 
