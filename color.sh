c48dcedbd18d2923aa7943e082ade4e19420cae0
henningfutrell <henning.futrell@protonmail.com>
2018-07-06 11:47:23 -0500

    Block ls colors being redundantly set on Mac. Only set on Linux.

--- color.sh
+++ color.sh
@@ -1,4 +1,9 @@
 export CLICOLOR=1
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
+
+if [[ `uname` == 'Linux' ]]; then
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+fi
+
+
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 export update_basically_powerful_zsh () {
     local current_dir=$PWD
     cd $z_core
-    git pull update master
+    git pull origin master
     git submodule update --init --recursive
     cd $current_dir
     unset current_dir
