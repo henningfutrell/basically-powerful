ce021c528344729674a9822cb8b12f6e9b38e34f
henningfutrell <henning.futrell@protonmail.comm>
2020-06-22 12:57:39 -0500

    Move setting nvm envars out of zshrc

--- zshrc.sh
+++ zshrc.sh
@@ -5,8 +5,5 @@ else
     z_core="$(dirname "$(readlink -f ~/.zshrc)")"
 fi
 
-source "$z_core/builder.sh"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source "$z_core/builder.sh"
